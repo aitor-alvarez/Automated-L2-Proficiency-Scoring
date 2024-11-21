aac7503a6cdc8af31d7064cdd00de08c84ff2fc9
Aitor Arronte Alvarez <arronte@hawaii.edu>
2024-11-20 14:52:00 -1000

    updated features

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,6 +1,11 @@
 import json
+import textstat
 import pandas as pd
 from generate_labels import get_scores
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+nlp.add_pipe("textdescriptives/all")
 
 def create_dataset(file_path):
     #dataset fields
@@ -17,23 +22,39 @@ def create_dataset(file_path):
     lexical_density=[]
     lexical_sophistication=[]
     lexical_variation=[]
-    mean_sentence_length=[]
+    sentence_length_mean=[]
+    sentence_length_std=[]
+    num_sentences=[]
+    dif_words=[]
+    dependency_distance_mean=[]
+    dependency_distance_std=[]
 
     #json file is ordered by session id by default
     f = json.load(open(file_path))
     df = pd.DataFrame.from_dict(f).sort_values(['participant', 'session_id'], ascending=[True, True])
     for d in df.iterrows():
         txt = '. '.join(d['user_response']).replace("\r\n","")
+        doc = nlp(txt)
         scores = get_scores(txt)
         session_id.append(d['session_id'])
         user_id.append(d['participant'])
         proficiency_level.append(d['proficiency_level'])
         date.append(d['session_start'][:10])
+        words=[word.text for word in doc]
+        content_words = [word.text for word in doc if word.pos_.startswith('VERB') or word.startswith('PROPN') or
+                         word.startswith('NOUN') or word.startswith('ADJ') or word.startswith('ADV')]
+
         #features
-        lexical_density.append()
+        lexical_density.append(len(content_words)/len(words))
         lexical_sophistication.append()
         lexical_variation.append()
-        mean_sentence_length.append()
+        sentence_length_mean.append(doc._.descriptive_stats['sentence_length_mean'])
+        num_sentences.append(doc._.descriptive_stats['n_sentences'])
+        sentence_length_std.append(doc._.descriptive_stats['sentence_length_std'])
+        dif_words.append(textstat.difficult_words(txt))
+        dependency_distance_mean.append(doc._.dependency_distance['dependency_distance_mean'])
+        dependency_distance_std.append(doc._.dependency_distance['dependency_distance_std'])
+
         #scores
         linguistic_range.append(scores['linguistic_range'])
         grammatical_accuracy.append(scores['grammatical_accuracy'])
@@ -46,10 +67,14 @@ def create_dataset(file_path):
     'lexical_density' : lexical_density,
     'lexical_sophistication' :lexical_sophistication,
     'lexical_variation' : lexical_variation,
-    'mean_sentence_length' : mean_sentence_length,
+    'num_sentences' : num_sentences,
+    'dif_words' : dif_words,
+    'sentence_length_mean': sentence_length_mean,
+    'sentence_length_std' : sentence_length_std,
+    'dependency_distance_mean': dependency_distance_mean,
+    'dependency_distance_std':dependency_distance_std,
     'linguistic_range': linguistic_range,
     'grammatical_accuracy': grammatical_accuracy
-
     })
 
     df.to_excel('dataset.xlsx')
