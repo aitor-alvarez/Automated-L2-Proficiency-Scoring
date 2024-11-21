f4f867d729d154777f40dbbc35557251e34dae18
Aitor Arronte Alvarez <arronte@hawaii.edu>
2024-11-21 10:17:50 -1000

    added reqs

--- generate_dataset.py
+++ generate_dataset.py
@@ -3,6 +3,7 @@ import textstat
 import pandas as pd
 from generate_labels import get_scores
 import spacy
+from taaled import ld
 
 nlp = spacy.load("en_core_web_sm")
 nlp.add_pipe("textdescriptives/all")
@@ -14,20 +15,20 @@ def create_dataset(file_path):
     user_id=[]
     date=[]
 
-    #scores
-    linguistic_range=[]
-    grammatical_accuracy=[]
-
     #features
     lexical_density=[]
-    lexical_sophistication=[]
     lexical_variation=[]
+    num_noun_chunks=[]
     sentence_length_mean=[]
     sentence_length_std=[]
     num_sentences=[]
     dif_words=[]
     dependency_distance_mean=[]
     dependency_distance_std=[]
+
+    # scores
+    linguistic_range = []
+    grammatical_accuracy = []
 
     #json file is ordered by session id by default
     f = json.load(open(file_path))
@@ -40,14 +41,16 @@ def create_dataset(file_path):
         user_id.append(d['participant'])
         proficiency_level.append(d['proficiency_level'])
         date.append(d['session_start'][:10])
+        num_chunks = len(set([chunk for chunk in doc.noun_chunks]))
         words=[word.text for word in doc]
         content_words = [word.text for word in doc if word.pos_.startswith('VERB') or word.startswith('PROPN') or
                          word.startswith('NOUN') or word.startswith('ADJ') or word.startswith('ADV')]
+        tokens = [word.text+'_'+word.pos_ for word in doc]
 
         #features
         lexical_density.append(len(content_words)/len(words))
-        lexical_sophistication.append()
-        lexical_variation.append()
+        lexical_variation.append(ld.lexdiv(tokens).mattr)
+        num_noun_chunks.append(num_chunks)
         sentence_length_mean.append(doc._.descriptive_stats['sentence_length_mean'])
         num_sentences.append(doc._.descriptive_stats['n_sentences'])
         sentence_length_std.append(doc._.descriptive_stats['sentence_length_std'])
@@ -65,7 +68,7 @@ def create_dataset(file_path):
     'session_id' : session_id,
     'user_id': user_id,
     'lexical_density' : lexical_density,
-    'lexical_sophistication' :lexical_sophistication,
+    'num_noun_chunks' : num_noun_chunks,
     'lexical_variation' : lexical_variation,
     'num_sentences' : num_sentences,
     'dif_words' : dif_words,
@@ -78,5 +81,5 @@ def create_dataset(file_path):
     })
 
     df.to_excel('dataset.xlsx')
-    print("dataset completed")
+    print("dataset generation completed")
     return None
