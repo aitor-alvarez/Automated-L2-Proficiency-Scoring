4e81ab94fc5030f08a5e1e70fe3075c186a5c4fc
Aitor Arronte Alvarez <arronte@hawaii.edu>
2024-12-20 15:58:47 -1000

    updated

---
+++ main.py
@@ -0,0 +1,14 @@
+from model import *
+from argparse import ArgumentParser
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    #model options are: 'rf', 'gbm', 'svm'
+    parser.add_argument('--model_name', type=str)
+    parser.add_argument('--dataset_file', type=str)
+    args = parser.parse_args()
+    if args.dataset_file and args.model_name:
+        dataset = dataset_preparation(args.dataset_file)
+        train_test_clf(dataset, args.model_name)
+    else:
+        print("Provide the correct parameters")
--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import roc_auc_score, f1_score
 from sklearn.multioutput import MultiOutputClassifier
+from sklearn.preprocessing import label_binarize
 import pandas as pd
 import numpy as np
 
@@ -9,12 +10,10 @@ def dataset_preparation(data_file):
     data = pd.read_excel(data_file)
     data = data.loc[:, ~data.columns.str.contains('^Unnamed')]
     data['user_id'], _ = pd.factorize(data['user_id'])
-    lr_index = data.columns.get_loc('linguistic_range')
-    ga_index = data.columns.get_loc('grammatical_accuracy')
-    y = data.iloc[:, [lr_index, ga_index]].to_numpy()
-    data = data.drop(['linguistic_range', 'grammatical_accuracy'], axis=1, inplace=True)
-    data = data.to_nunmpy()
-    x_train, x_test, y_train, y_test = train_test_split(data, y, test_size=.2)
+    y = data[['linguistic_range', 'grammatical_accuracy']]
+    data.drop(['linguistic_range', 'grammatical_accuracy'], axis=1, inplace=True)
+    data = data.to_numpy()
+    x_train, x_test, y_train, y_test = train_test_split(data,  y, test_size=.2)
     return [x_train, x_test, y_train, y_test]
 
 def train_test_clf(data, model_name):
@@ -26,7 +25,10 @@ def train_test_clf(data, model_name):
     # iterate over the y dimension
     f1_scores=[]
     auc_scores=[]
-    for i in range(y_test.shape[1]):
+    labels = list(set(y_test))
+    ytest = label_binarize(y_test, classes=labels)
+    ypreds = label_binarize(preds, classes=labels)
+    for i in range(ytest.shape[1]):
         f1_scores.append(f1_score(y_test[:, i], preds[:,i]))
         auc_scores.append(roc_auc_score(y_test[:, i], preds[:,i]))
 
@@ -36,22 +38,21 @@ def train_test_clf(data, model_name):
 
 def find_parameters(model_name, X, y):
     if model_name =='rf':
-        model = MultiOutputClassifier(RandomForestClassifier())
+        model = RandomForestClassifier()
         params = {
             'n_estimators': [100, 200, 300, 500],
-            'max_features': ['auto', 'sqrt', 'log2'],
-            'max_depth': [4, 5, 6, 7, 8],
-            'criterion': ['friedman_mse']
+            'max_features': ['sqrt', 'log2'],
+            'max_depth': [4, 5, 6, 7, 8]
         }
         grdsearch = GridSearchCV(estimator=model, param_grid=params)
         grdsearch.fit(X, y)
         best_params = grdsearch.best_params_
         model_optim = RandomForestClassifier(n_estimators=best_params['n_estimators'],
                                           max_features=best_params['max_features'],
-                                          max_depth=best_params['max_depth'], criterion=best_params['criterion'])
+                                          max_depth=best_params['max_depth'])
 
     elif model_name =='gbm':
-        model = MultiOutputClassifier(GradientBoostingClassifier())
+        model = GradientBoostingClassifier()
         params = {
                 'loss': ['log_loss', 'exponential'],
                 'n_estimators': [100, 200, 300, 500],
