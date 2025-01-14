b9d22c84284d78d11ccfb8155540d23b265374ee
Aitor Arronte Alvarez <arronte@hawaii.edu>
2025-01-13 16:08:30 -1000

    added verbose off

--- model.py
+++ model.py
@@ -3,17 +3,24 @@ from sklearn.feature_selection import RFE, SelectKBest, chi2, mutual_info_classi
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import roc_auc_score, f1_score, accuracy_score
 from sklearn.multioutput import MultiOutputClassifier
-from sklearn.preprocessing import label_binarize
+from sklearn.preprocessing import label_binarize, LabelEncoder
 from xgboost.sklearn import XGBClassifier
+import warnings
 import lightgbm as lgb
 import pandas as pd
 import numpy as np
+
+warnings.filterwarnings("ignore")
 
 def dataset_preparation(data_file):
     data = pd.read_excel(data_file)
     data = data.loc[:, ~data.columns.str.contains('^Unnamed')]
     data['user_id'], _ = pd.factorize(data['user_id'])
     y = data[['linguistic_range', 'grammatical_accuracy']].to_numpy()
+    # For xgb now it is required for labels to start at index 0
+    le = LabelEncoder()
+    y = [le.fit_transform(y[:,0]),le.fit_transform(y[:,1])]
+    y = np.stack( y, axis=1)
     data.drop(['linguistic_range', 'grammatical_accuracy'], axis=1, inplace=True)
     #We keep the original data for feature selection (column names)
     data2 = data.to_numpy()
@@ -28,12 +35,12 @@ def train_test_clf(data_train, model_name):
     clf = MultiOutputClassifier(model_optim).fit(x_train, y_train)
     preds = clf.predict(x_test)
     # iterate over the y dimension
-    labels = list(set(y_train[:,0]))
     f1_scores = []
     auc_scores = []
     accuracy_scores = []
-    ytest = label_binarize(list(y_test[:,0]), classes=labels)
     for i in range(y_test.shape[1]):
+        labels = list(set(y_train[:, i]))
+        ytest = label_binarize(list(y_test[:, i]), classes=labels)
         ypreds = label_binarize(list(preds[:,i]), classes=labels)
         f1_scores.append(f1_score(y_test[:, i], preds[:,i], average='weighted'))
         accuracy_scores.append(accuracy_score(y_test[:, i], preds[:,i]))
@@ -60,17 +67,16 @@ def find_parameters(model_name, X, y):
                                           max_depth=best_params['max_depth'])
 
     elif model_name == 'lgb':
-        model = lgb.LGBMClassifier(num_leaves=35, learning_rate=0.01, n_estimators=20)
+        model = lgb.LGBMClassifier(num_leaves=35, learning_rate=0.01, n_estimators=20, verbose=-1)
         params = {"learning_rate": [0.01, 0.1, 0.3], "n_estimators": [20, 40, 60]}
         grdsearch = GridSearchCV(estimator=model, param_grid=params)
         grdsearch.fit(X, y)
         best_params = grdsearch.best_params_
-        model_optim = lgb.LGBMClassifier(num_leaves=35, learning_rate=best_params['learning_rate'], n_estimators=best_params['n_estimators'])
+        model_optim = lgb.LGBMClassifier(num_leaves=35, learning_rate=best_params['learning_rate'], n_estimators=best_params['n_estimators'], verbose=-1)
 
     elif model_name =='gbm':
-        model = GradientBoostingClassifier()
+        model = GradientBoostingClassifier(loss='log_loss', verbose=-1)
         params = {
-                'loss': ['log_loss', 'exponential'],
                 'n_estimators': [100, 200, 300, 500],
                 'max_features': ['sqrt', 'log2'],
                 'max_leaf_nodes': [2, 4, 6, 8]
@@ -78,22 +84,22 @@ def find_parameters(model_name, X, y):
         grdsearch = GridSearchCV(estimator=model, param_grid=params)
         grdsearch.fit(X, y)
         best_params = grdsearch.best_params_
-        model_optim = GradientBoostingClassifier(loss= best_params['loss'],
+        model_optim = GradientBoostingClassifier(loss= 'log_loss',
                                         n_estimators=best_params['n_estimators'],
                                           max_features=best_params['max_features'],
-                                          max_leaf_nodes=best_params['max_leaf_nodes'])
+                                          max_leaf_nodes=best_params['max_leaf_nodes'], verbose=-1)
 
     elif model_name =='xgb':
         model = XGBClassifier(learning_rate =0.1, min_child_weight=1,
                               gamma=0, subsample=0.8, colsample_bytree=0.8,
-                              objective= 'multi:softmax', nthread=4,scale_pos_weight=1, seed=27)
+                              objective= 'multi:softmax', nthread=4, seed=27, verbose=-1)
         params = {"max_depth": [2, 4, 6], "n_estimators": [50, 100, 200, 500, 750]}
         grdsearch = GridSearchCV(estimator=model, param_grid=params)
         grdsearch.fit(X, y)
         best_params = grdsearch.best_params_
         model_optim = XGBClassifier(learning_rate = 0.1, max_depth = best_params['max_depth'], min_child_weight=1,
                               gamma=0, subsample=0.8, colsample_bytree=0.8, n_estimators = best_params['n_estimators'],
-                              objective= 'multi:softmax', nthread=4,scale_pos_weight=1, seed=27)
+                              objective= 'multi:softmax', nthread=4,scale_pos_weight=1, seed=27, verbose=-1)
 
     return model_optim
 
