a5995be8494645e9b44dcaaf2fcd807187515a14
aitor-alvarez <aitoraalvarez@gmail.com>
2025-08-07 11:01:41 -1000

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 ## Repository for Automated L2 Proficiency Scoring: Weak Supervision, Large Language Models, and Statistical Guarantees
+
+### Install requirements
+
+```
+pip install -r requirements.txt
+```
+
+### Obtain scores from GPT models
+
+```
+python generate_dataset.py --model_name 'gpt-4o' --dataset_file' 'json_file_with conversational_responses'
+```
+
+### Train models and obtain C.I. with PPI in a semisupervised regime
+
+```
+python main.py --model_name 'xgb' --dataset_file' 'path to excel or csv file with dataset generated in the previous step' \
+--dataset_unl 'path to csv or excel with features but no labels' --training 'semi' --model_params 'dict with xgb params'
+```
--- main.py
+++ main.py
@@ -5,14 +5,29 @@ from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    #model options are: 'rf', 'gbm', 'xgb'
+    #model options are:  'lgbm', 'xgb'
     parser.add_argument('--model_name', type=str)
     parser.add_argument('--dataset_file', type=str)
-    parser.add_argument('--feature_set', type=str)
+    #Unlabeled dataset
+    parser.add_argument('--dataset_unl', type=str)
+    #training= 'weak' or 'semi'
+    parser.add_argument('--training', type=str)
+    #Model parameters
+    parser.add_argument('--model_params', type=str)
     args = parser.parse_args()
-    if args.dataset_file and args.model_name:
+    if args.dataset_file and args.model_name and args.training:
+        #labeled dataset
         dataset = dataset_preparation(args.dataset_file)
-        train_test_clf(dataset, args.model_name)
+        unlabeled_data =args.dataset_unl
+        if args.training =='semi':
+            semi_supervised_ppi_train(dataset, unlabeled_data, args.model_name, args.model_params, sample_size=100, alpha=0.1,
+                                      w_t=0.2, max_sample_size=1100)
+        elif args.training == 'weak':
+            weakly_supervised_ppi_train(dataset, unlabeled_data, args.model_name, args.model_params, sample_size=100, alpha=0.1,
+                                        w_t=0.2, max_sample_size=1100)
+        else:
+            print("revise the arguments required to run the model")
+
     #Feature selection
     elif args.feature_set:
         data = pd.read_excel(args.feature_set)
